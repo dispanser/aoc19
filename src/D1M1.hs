31920024c195ed56d022049ac5fb1f1772757895
Thomas Peiselt <pi@kulturguerilla.org>
2019-12-01 09:40:09 +0100

    Advent of Code 2019: Day 1, Module 1.

---
+++ src/D1M1.hs
@@ -0,0 +1,14 @@
+module D1M1 where
+
+import System.IO (readFile)
+
+main :: IO ()
+main = do
+    ls <- lines <$> readFile "data/d1m1.txt"
+    -- let xs = (read :: String -> Int) <$> lines
+    -- vals <- readFile "data/d1m1.txt"
+    let ws = (read :: String -> Int) <$> ls
+    print . sum $ map fuel ws
+
+fuel :: Int -> Int
+fuel = subtract 2 . (`div` 3)
