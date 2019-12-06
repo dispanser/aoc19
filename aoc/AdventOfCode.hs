7e3c1cb7b2f91db18b51bd4b818ba1de05c8eba6
Thomas Peiselt <pi@kulturguerilla.org>
2019-12-06 09:56:07 +0100

    Day 5: add to main

--- aoc/AdventOfCode.hs
+++ aoc/AdventOfCode.hs
@@ -8,6 +8,7 @@ import qualified Data.List.Split as S
 import qualified Day2
 import qualified Day3
 import qualified Day4
+import qualified Day5
 import Data.Vector.Unboxed (fromList, (//))
 
 main :: IO ()
@@ -28,8 +29,8 @@ day2 :: IO ()
 day2 = do
     intCodeIn  <- fromList <$> readIntList "data/day2.txt"
     let intCodeMod = intCodeIn // [(1, 12), (2, 2)]
-    putStrLn $ "day 2, part #1: " ++ show (Day2.part1' intCodeMod)
-    putStrLn $ "day 2, part #2: " ++ show (Day2.part2' intCodeIn 19690720)
+    putStrLn $ "day 2, part #1: " ++ show (Day2.part1 intCodeMod)
+    putStrLn $ "day 2, part #2: " ++ show (Day2.part2 intCodeIn 19690720)
 
 day3 :: IO ()
 day3 = do
@@ -41,6 +42,11 @@ day4 :: IO ()
 day4 = do
     putStrLn $ "day4, part #1: " ++ show (Day4.part1 245182 790572)
     putStrLn $ "day4, part #2: " ++ show (Day4.part2 245182 790572)
+
+day5 :: IO ()
+day5 = do
+    intCodeIn  <- fromList <$> readIntList "data/day5.txt"
+    putStrLn $ "day 5, part #1: " ++ show (Day5.part1 intCodeIn [1])
 
 readIntLines :: FilePath -> IO [Int]
 readIntLines f = do
