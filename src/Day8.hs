5e04c2f1691cafde16e977a4c0fd69259c9eb8c4
Thomas Peiselt <pi@kulturguerilla.org>
2019-12-08 17:30:13 +0100

    little refactoring Day8.
    Remove the notion of rows and columns, as it's not needed for any of the
    two parts :)

--- src/Day8.hs
+++ src/Day8.hs
@@ -5,7 +5,8 @@ import Data.List.Split (chunksOf)
 import Data.List (sortOn)
 import qualified Data.Vector.Unboxed as V
 
-newtype Layer = Layer [V.Vector Int] deriving (Eq, Show)
+newtype Layer = Layer {unlayer :: V.Vector Int} deriving (Eq, Show)
+
 
 -- |
 -- >>> part1 2 3 "034122001122"
@@ -22,27 +23,23 @@ part1 rows cols inp =
 
 -- | count occurrences of given digit in the layer
 --
--- >>> countDigit 5 $ Layer [V.fromList [5,5,4],V.fromList [1,1,1]]
+-- >>> countDigit 5 $ Layer $ V.fromList [5,5,4,1,1,1]
 -- 2
--- >>> countDigit 0 $ Layer [V.fromList [5,5,4],V.fromList [1,1,1]]
+-- >>> countDigit 0 $ Layer $ V.fromList [5,5,4,1,1,1]
 -- 0
--- >>> countDigit 1 $ Layer [V.fromList [5,5,4],V.fromList [1,1,1]]
+-- >>> countDigit 1 $ Layer $ V.fromList [5,5,4,1,1,1]
 -- 3
 countDigit :: Int -> Layer -> Int
-countDigit digit (Layer ls) = sum $ V.length . V.filter (==digit) <$> ls
+countDigit digit = V.length . V.filter (==digit) . unlayer
 
 -- |
 --
 -- >>> decodeStream 2 3 "123456789012"
--- [Layer [[1,2,3],[4,5,6]],Layer [[7,8,9],[0,1,2]]]
+-- [Layer {unlayer = [1,2,3,4,5,6]},Layer {unlayer = [7,8,9,0,1,2]}]
 decodeStream :: Int -> Int -> String -> [Layer]
-decodeStream rows cols inp = decodeSingle cols <$> chunksOf (rows * cols) inp
+decodeStream rows cols inp = decodeSingle <$> chunksOf (rows * cols) inp
+ where
+  decodeSingle :: String -> Layer
+  decodeSingle xs = Layer . V.fromList $ digitToInt <$> xs
 
--- |
---
--- >>> decodeSingle 3 "654321"
--- Layer [[6,5,4],[3,2,1]]
-decodeSingle :: Int -> String -> Layer
-decodeSingle cols inp = Layer $ parseLayerLine <$> chunksOf cols inp
-  where parseLayerLine :: String -> V.Vector Int
-        parseLayerLine l = V.fromList $ digitToInt <$> l
+
